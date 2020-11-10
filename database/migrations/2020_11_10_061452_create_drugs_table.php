5548ed5b24ce4be6be04411e11d1be25be3deee7
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-10 13:40:53 +0700

    Make create_drugs_table migration

---
+++ database/migrations/2020_11_10_061452_create_drugs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDrugsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drugs', function (Blueprint $table) {
+            $table->bigIncrements('drug_id');
+            $table->string('drug_name', 255);
+            $table->bigInteger('drug_price');
+            $table->bigInteger('drug_stock');
+            $table->text('drug_description');
+            $table->string('drug_image', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drugs');
+    }
+}

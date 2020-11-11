995461c7707aca4b1cfb4733fa750385c75ce9c5
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-11 16:22:44 +0700

    create_users_table

---
+++ database/migrations/2020_11_11_091545_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->string('user_name', 255);
+            $table->string('user_username', 255);
+            $table->string('user_password', 255);
+            $table->string('user_email', 255);
+            $table->integer('user_role');
+            $table->string('user_image', 255);
+            $table->timestamps();
+            $table->foreign('user_role')->references('role_id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

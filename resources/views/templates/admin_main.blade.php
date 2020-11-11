3a57ed6e2036b1147963489d28f6e01baf5c422b
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-11 15:18:45 +0700

    FIx Drug Datatables

--- resources/views/templates/admin_main.blade.php
+++ resources/views/templates/admin_main.blade.php
@@ -16,15 +16,16 @@
     <link rel="apple-touch-icon" href="apple-icon.png">
     <link rel="shortcut icon" href="favicon.ico">
 
-    <link rel="stylesheet" href="{{ URL::asset('assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/vendors/themify-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/vendors/selectFX/css/cs-skin-elastic.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/vendors/jqvmap/dist/jqvmap.min.css') }}">
+    <link rel="stylesheet" href="{{URL::asset('assets/vendors/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/vendors/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/vendors/themify-icons/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/vendors/selectFX/css/cs-skin-elastic.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/vendors/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/vendors/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css')}}">
 
+    <link rel="stylesheet" href="{{URL::asset('assets/css/style.css')}}">
 
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
@@ -100,35 +101,22 @@
 
         @yield('page')
 
-        <script src="{{ URL::asset('assets/vendors/jquery/dist/jquery.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/vendors/popper.js/dist/umd/popper.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/js/main.js') }}"></script>
+        <script src="{{URL::asset('assets/vendors/jquery/dist/jquery.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/popper.js/dist/umd/popper.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        <script src="{{URL::asset('assets/assets/js/main.js')}}"></script>
 
-        <script src="{{ URL::asset('assets/vendors/chart.js/dist/Chart.bundle.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/js/dashboard.js') }}"></script>
-        <script src="{{ URL::asset('assets/js/widgets.js') }}"></script>
-        <script src="{{ URL::asset('assets/vendors/jqvmap/dist/jquery.vmap.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js') }}"></script>
-        <script src="{{ URL::asset('assets/vendors/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
-        <script>
-            (function($) {
-                "use strict";
-
-                jQuery('#vmap').vectorMap({
-                    map: 'world_en',
-                    backgroundColor: null,
-                    color: '#ffffff',
-                    hoverOpacity: 0.7,
-                    selectedColor: '#1de9b6',
-                    enableZoom: true,
-                    showTooltip: true,
-                    values: sample_data,
-                    scaleColors: ['#1de9b6', '#03a9f5'],
-                    normalizeFunction: 'polynomial'
-                });
-            })(jQuery);
-        </script>
+        <script src="{{URL::asset('assets/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/jszip/dist/jszip.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/pdfmake/build/pdfmake.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/pdfmake/build/vfs_fonts.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+        <script src="{{URL::asset('assets/vendors/datatables.net-buttons/js/buttons.colVis.min.js')}}"></script>
+        <script src="{{URL::asset('assets/js/init-scripts/data-table/datatables-init.js')}}"></script>
 
 
 </body>

ace15ac59d7cab6123294f516e6fba4e9a135db3
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-12 13:52:44 +0700

    Fix Navigation under Landing Navbar

--- resources/views/landing/etalase.blade.php
+++ resources/views/landing/etalase.blade.php
@@ -6,7 +6,7 @@
 <div class="bg-light py-3">
   <div class="container">
     <div class="row">
-      <div class="col-md-12 mb-0"><a href="">Exodus Drugstore</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Etalase</strong></div>
+      <div class="col-md-12 mb-0"><a href="{{url('/')}}">Exodus Drugstore</a> <span class="mx-2 mb-0">/</span><strong class="text-black">Etalase</strong></div>
     </div>
   </div>
 </div>

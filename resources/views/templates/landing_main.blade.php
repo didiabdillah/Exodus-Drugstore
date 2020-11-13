65ec929b027197d196692f8935954b3d4968539b
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-13 13:56:31 +0700

    Add Favicon

--- resources/views/templates/landing_main.blade.php
+++ resources/views/templates/landing_main.blade.php
@@ -6,6 +6,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+  <link rel="shortcut icon" href="{{URL::asset('assets/img/favicon.ico')}}">
+
   <link href="https://fonts.googleapis.com/css?family=Rubik:400,700|Crimson+Text:400,400i" rel="stylesheet">
   <link rel="stylesheet" href="{{ URL::asset('assets/landing/fonts/icomoon/style.css') }}">
 

143e040f13d419e664dbf0dd0cce9bb7a1922c70
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-20 15:11:37 +0700

    fitur insert delivery di admin

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -14,4 +14,28 @@ class DeliveryController extends Controller
 
         return view('admin/delivery', compact('delivery'));
     }
+
+    public function insert()
+    {
+        return view('admin/insert_delivery');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'delivery_name'  => 'required',
+            'service_price'  => 'required',
+            'service_name'  => 'required'
+        ]);
+
+        $data = [
+            'delivery_name' => $request->delivery_name,
+            'service_price' => $request->service_price,
+            'service_name' => $request->service_name
+        ];
+
+        DB::table('delivery_services')->insert($data);
+
+        return redirect('/delivery');
+    }
 }
